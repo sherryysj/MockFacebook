45373ea80ee110a1159931cff3ce81e8696de1af
sherryysj <45263452+sherryysj@users.noreply.github.com>
2021-12-10 00:34:19 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<html lang="en">
+ <head>
+   <title>Mock Facebook</title>
+   <meta charset = "utf-8">
+ </head>
+
+
+ <body>
+   <h1> Welcome to Mock Facebook</h1>
+   <a> You have to sign in to see the contents of the website. </a>
+   <a> If you do not have an account, please click sign up to register one. </a><br>
+   <a> </a>
+   <form action="loginValidate.php" method="post">
+
+   Email: <input type="text" name="Email"><br>
+   Password: <input type="text" name="Password"><br>
+
+   <input type="submit" value="Sign In">
+   </form>
+
+   <form action="signup.php">
+   <input style="display:inline-block;" type="submit" value="Sign Up">
+   <form>
+
+</body>
+
+</html>
+
