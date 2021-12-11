4d8a42231b68d79a669feecf4d270a3f594b83c8
sherryysj <45263452+sherryysj@users.noreply.github.com>
2021-12-11 15:48:01 +1100

    Update UI layout for the login form

--- index.php
+++ index.php
@@ -41,37 +41,37 @@
         <h3 class="h3 mb-2 fw-normal">For demo purpose, you can use username: DemoUser, password: Demo to login. Or you can create an new account to test the function if you are willing to.</h2>
       </div>
 
+      <main class="form-signin">
 
+<a> </a>
+<form>
+
+ <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+ <div class="form-floating">
+   <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+   <label for="floatingInput">Email address</label>
+ </div>
+ <div class="form-floating">
+   <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+   <label for="floatingPassword">Password</label>
+ </div>
+
+ <div class="checkbox mb-3">
+   <label>
+     <input type="checkbox" value="remember-me"> Remember me
+   </label>
+ </div>
+ <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+
+ </form>
+
+<form action="signup.php">
+ <button class="w-100 btn btn-lg btn-primary" type="submit">Sign Up</button>
+</form>
+</main>
     </div>
- <main class="form-signin">
-
-   <a> </a>
-   <form>
-
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
-
-    <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-
-    </form>
-
-   <form action="signup.php">
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign Up</button>
-   </form>
-   </main>
+ 
 </body>
 
 </html>
