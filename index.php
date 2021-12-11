8a2fe4e8716d4c8239e94a4c2c5e7c6e27f8b547
sherryysj <45263452+sherryysj@users.noreply.github.com>
2021-12-11 15:55:50 +1100

    clear coding style

--- index.php
+++ index.php
@@ -38,38 +38,44 @@
         <h1 class="h1 mb-2 fw-normal">Welcome to Mock Facebook</h1>
         <h2 class="h2 mb-2 fw-normal">You have to sign in to see the contents of the website.</h2>
         <h2 class="h2 mb-5 fw-normal">If you do not have an account, please click sign up to register one.</h2>
-        <h3 class="h3 mb-2 fw-normal">For demo purpose, you can use username: DemoUser, password: Demo to login. Or you can create an new account to test the function if you are willing to.</h2>
+        <h3 class="h3 mb-2 fw-normal">For demo purpose, you can use Username: DemoUser, Password: Demo to login. </h3>
+        <h3 class="h3 mb-5 fw-normal">Or you can create a new account to test the sign up function if you are willing to so.</h3>
       </div>
 
       <main class="form-signin">
 
-<a> </a>
-<form>
+        <a> </a>
+        <form>
 
- <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+          <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
- <div class="form-floating">
-   <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-   <label for="floatingInput">Email address</label>
- </div>
- <div class="form-floating">
-   <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-   <label for="floatingPassword">Password</label>
- </div>
+          <div class="form-floating">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+          </div>
 
- <div class="checkbox mb-3">
-   <label>
-     <input type="checkbox" value="remember-me"> Remember me
-   </label>
- </div>
- <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+          </div>
 
- </form>
+          <div class="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me"> Remember me
+            </label>
+          </div>
 
-<form action="signup.php">
- <button class="w-100 btn btn-lg btn-primary" type="submit">Sign Up</button>
-</form>
-</main>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+
+        </form>
+
+        <form action="signup.php">
+          <a> </a>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign Up</button>
+        </form>
+
+      </main>
+
     </div>
  
 </body>
